7d9457081af4c17ceb918e733ea0dba4cbee4267
Max <civnopes@gmail.com>
2022-07-08 21:57:38 +0200

    Fetching questions on dashboard

--- mood-meter/src/pages/Dashboard.tsx
+++ mood-meter/src/pages/Dashboard.tsx
@@ -1,16 +1,34 @@
 import Layout from "../components/layout";
 import { Link } from "react-router-dom";
 import TestDiagram from "./TestDiagram";
+import { useEffect, useState } from "react";
 
 export default function DashboardView() {
-  const testQuestionId = "123";
-  return (
-    <Layout>
-      <TestDiagram width={500} height={500} />
+  const [questions, setQuestions] = useState();
 
-      <button className="btn btn-primary">
-        <Link to={`rate/${testQuestionId}`}>Bewerten</Link>
-      </button>
-    </Layout>
-  );
+  useEffect(() => {
+    getQuestionsData();
+  }, []);
+
+  const getQuestionsData = async () => {
+    const response = await fetch("/questions").then((response) =>
+      response.json()
+    );
+
+    setQuestions(response);
+  };
+
+  if (questions) {
+    return (
+      <Layout>
+        <TestDiagram width={500} height={500} />
+
+        <button className="btn btn-primary">
+          <Link to={`rate/${questions[0]["id"]}`}>Bewerten</Link>
+        </button>
+      </Layout>
+    );
+  } else {
+    return <></>;
+  }
 }
